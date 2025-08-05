776679ee2485fe1145b8b84bb73209a0e94a985d
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-05 21:12:00 +0530

    Page-title-generateMetadata basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import type { Metadata } from "next";
+
+export const generateMetadata: () => Metadata = () => ({
+	title: "Prepsage | All",
+	description: "",
+	keywords: "",
+});
+
 const page = () => {
-  return (
-    <>
-      <div className="">Home Page</div>
-    </>
-  );
+	return (
+		<>
+			<div className="">Home Page</div>
+		</>
+	);
 };
 
 export default page;
